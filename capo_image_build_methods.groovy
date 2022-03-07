579b2fda5929f45674b74dd5fd4716a23f7a324e
Jiang Wu <john.x.wu@ericsson.com>
2022-03-07 22:36:10 +0200

    update code Mon Mar  7 22:36:10 EET 2022

--- capo_image_build_methods.groovy
+++ capo_image_build_methods.groovy
@@ -36,21 +36,21 @@ void buildValuePackage(int stageTimeout, String timeoutUnits='MINUTES')
 {
     String stageName = "Build Value Package"
     def stageAction = {
-        dir(env.BM_CONTAINER_LIST_WORKDIR) {
+        dir(env.CAPO_CONTAINER_LIST_WORKDIR) {
             // Build them with python script
             // The script saves a JSON list to BM_VALUEPACK_JSON
             sh '''#!/bin/bash -xe
-                echo "${WORKSPACE}/builder/value_packs.py  build  --container-list container-list.json  >> ${BM_VALUEPACK_JSON}"
+                echo "${WORKSPACE}/builder/value_packs.py  build  --container-list container-list.json  >> ${CAPO_VALUEPACK_JSON}"
                 echo "create draino_v1.0.tar.gz"
                 touch draino_v1.0.tar.gz
                 for i in *.gz; do
                     [ -f "$i" ] || break
-                    echo upload /baremetal/valuepacks/${ERIKUBE_TAG%%-*} $i
+                    echo "upload /baremetal/valuepacks/${ERIKUBE_TAG%%-*} $i"
                 done
                 '''.stripIndent()
             ccd_rel_version = env.ERIKUBE_TAG.split("-")[0]
-            env.VALUEPACKS_URL = "${env.ARTIFACTORY_BASE_PATH}/baremetal/valuepacks/${ccd_rel_version}"
-            def valuePacksJson = readFile("${env.BM_VALUEPACK_JSON}")
+            env.VALUEPACKS_URL = "${env.ARTIFACTORY_BASE_PATH}/capo/valuepacks/${ccd_rel_version}"
+            def valuePacksJson = readFile("${env.CAPO_VALUEPACK_JSON}")
             def summary = createSummary(icon: 'package.png', text: "Value packs:<br>")
             summary.appendText(valuePacksJson)
         }
--- env.groovy
+++ env.groovy
@@ -2,6 +2,7 @@ env.K8S_COMMON_METHODS = "k8s_image_build_methods.groovy"
 env.CAPO_COMMON_METHODS = "capo_image_build_methods.groovy"
 env.ERIKUBE_TAG = "eri_tag"
 env.VARIABLE_FILE = "${WORKSPACE}/variables.txt"
+env.CAPO_CONTAINER_LIST_WORKDIR = "${WORKSPACE}/kube-deployment/ansible/common"
 env.CAPO_VALUEPACK_JSON = "${WORKSPACE}/valuepacks.json"
 env.ARTIFACTORY_BASE_PATH = "https://host/artifactory/proj-erikube-generic-local/erikube"
 
