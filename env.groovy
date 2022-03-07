f7edbcbd22803e526513fc9a14b089025ccc9ddc
Jiang Wu <john.x.wu@ericsson.com>
2022-03-07 22:26:48 +0200

    update code Mon Mar  7 22:26:48 EET 2022

--- env.groovy
+++ env.groovy
@@ -2,3 +2,6 @@ env.K8S_COMMON_METHODS = "k8s_image_build_methods.groovy"
 env.CAPO_COMMON_METHODS = "capo_image_build_methods.groovy"
 env.ERIKUBE_TAG = "eri_tag"
 env.VARIABLE_FILE = "${WORKSPACE}/variables.txt"
+env.CAPO_VALUEPACK_JSON = "${WORKSPACE}/valuepacks.json"
+env.ARTIFACTORY_BASE_PATH = "https://host/artifactory/proj-erikube-generic-local/erikube"
+
